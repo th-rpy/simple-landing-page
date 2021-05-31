b80e35a0223eaef25760043e73e4064a23ac6911
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-05-31 01:04:29 -0400

    ADD: JS, CSS and Html Files

---
+++ app.js
@@ -0,0 +1,43 @@
+(function() {
+
+    const target = document.querySelector(".target");
+    const links = document.querySelectorAll(".mynav a");
+    const colors = [ "magenta", "black", "darkblue"];
+    const cover = document.getElementById("cover");
+    const div = document.getElementById("card"); 
+  
+    function mouseenterFunc() {
+      if (!this.parentNode.classList.contains("active")) {
+        for (let i = 0; i < links.length; i++) {
+          if (links[i].parentNode.classList.contains("active")) {
+            links[i].parentNode.classList.remove("active");
+          }
+          links[i].style.opacity = "0.25";
+        }
+  
+        this.parentNode.classList.add("active");
+        this.style.opacity = "1";
+  
+        const width = this.getBoundingClientRect().width;
+        const height = this.getBoundingClientRect().height;
+        const left = this.getBoundingClientRect().left;
+        const top = this.getBoundingClientRect().top;
+        const color = colors[Math.floor(Math.random() * colors.length)];
+  
+        target.style.width = `${width}px`;
+        target.style.height = `${height}px`;
+        target.style.left = `${left}px`;
+        target.style.top = `${top}px`;
+        target.style.borderColor = color;
+        target.style.transform = "none";
+      }
+    }
+  
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", (e) => e.preventDefault());
+      links[i].addEventListener("mouseenter", mouseenterFunc);
+    }
+
+    
+  })();
+  
